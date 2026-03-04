2cbd3a5e608c060497a10ed0a68a2875a519d11a
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-04 11:03:31 -0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,13 +1,14 @@
 
-import random
+import random / estamos adicionando a Biblioteca 
 
-numero = random.randint(1, 10)
+numero = random.randint(1, 10) / escolher um n° entre 1 e 10.
 
-chute = int(input("Adivinhe um n° de 1 a 10: "))
+chute = int(input("Adivinhe um n° de 1 a 10: ")) / o int reconhece o n°, input possibilita que o jogador digite o n°.
 
-if chute == numero:
+if chute == numero: / se for verdadeiro vai mandar para o jogador uma mensagem "Você acertou".
     print("Você acertou!!") 
-else: 
+
+else:  / se errar, vai mandar a mensagem "Você Errou!!!" e mostrar o n° correto.
     print("Você Errou!!!") 
     print("O n° era:", numero)
 
